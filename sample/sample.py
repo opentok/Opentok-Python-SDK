1ddf40eca13c5cba1c08561656270b9871469d60
Mike Kascel <mkascel@gmail.com>
2011-10-26 13:59:27 -0700

    minor edit

--- sample/sample.py
+++ sample/sample.py
@@ -1,15 +1,15 @@
 import OpenTokSDK
 
 api_key = '5875231' # Replace with your OpenTok API key.
-api_secret = "8f5cde4ade6b11ea22cfd73ea345c64b4e423d29"  # Replace with your OpenTok API secret.
-session_address = "192.0.43.10" # Replace with the representative URL of your session.
+api_secret = '8f5cde4ade6b11ea22cfd73ea345c64b4e423d29'  # Replace with your OpenTok API secret.
+session_address = '192.0.43.10' # Replace with the representative URL of your session.
 
 opentok_sdk = OpenTokSDK.OpenTokSDK(api_key, api_secret)
 session = opentok_sdk.create_session(session_address)
 
 print session.session_id
 
-connectionMetadata = "username=Bob,userLevel=4"
+connectionMetadata = 'username=Bob, userLevel=4'
 token = opentok_sdk.generate_token(session.session_id, 'PUBLISHER', None, connectionMetadata)
 
 print token
