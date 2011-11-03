3f5def49d162aa0a3af6a51896a979556ea47ac2
Randall Degges <rdegges@gmail.com>
2011-11-03 15:45:54 -0700

    Adding a setup.py file.
    This will allow your OpenTok library to be distributed via the python package
    manager, (pypi: http://pypi.python.org/pypi).
    
    In order to register your library for public consumption, you can run:
    
        python setup.py sdist register upload
    
    From the CLI under your company account.

---
+++ setup.py
@@ -0,0 +1,36 @@
+from os.path import abspath, dirname, join, normpath
+
+from setuptools import setup
+
+
+setup(
+
+    # Basic package information:
+    name = 'opentok-python-sdk',
+    version = '0.91.0',
+    py_modules = ['OpenTokSDK'],
+
+    # Packaging options:
+    zip_safe = False,
+    include_package_data = True,
+
+    # Metadata for PyPI:
+    author = 'TokBox, Inc.',
+    author_email = 'mkascel@gmail.com',
+    #license = '?',
+    url = 'https://github.com/opentok/Opentok-Python-SDK',
+    keywords = 'video chat tokbox tok opentok python media',
+    description = 'A python wrapper for the OpenTok video chat APIs.',
+    long_description = open(normpath(join(dirname(abspath(__file__)),
+        'README'))).read(),
+    classifiers = [
+        'Intended Audience :: Developers',
+        'Topic :: Communications',
+        'Topic :: Communications :: Chat',
+        'Topic :: Communications :: Conferencing',
+        'Topic :: Multimedia :: Video :: Capture',
+        'Topic :: Multimedia :: Video :: Display',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+
+)
