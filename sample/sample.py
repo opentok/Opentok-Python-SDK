f4adee3418ce01e5af5418f6db4f487c42e400d2
Ben Pedrick <ben@tokbox.com>
2012-08-28 12:24:59 -0700

    Remove staging flag from sample

--- sample/sample.py
+++ sample/sample.py
@@ -4,7 +4,7 @@ api_key = '5875231' # Replace with your OpenTok API key.
 api_secret = '8f5cde4ade6b11ea22cfd73ea345c64b4e423d29'  # Replace with your OpenTok API secret.
 session_address = '192.0.43.10' # Replace with the representative URL of your session.
 
-opentok_sdk = OpenTokSDK.OpenTokSDK(api_key, api_secret, staging=True)
+opentok_sdk = OpenTokSDK.OpenTokSDK(api_key, api_secret)
 session = opentok_sdk.create_session(session_address)
 
 print session.session_id
