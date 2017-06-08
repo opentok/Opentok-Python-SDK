a34bddfdf533bfb333e45aa68c9e775c859337dc
Aiham Hammami <aiham@aiham.net>
2017-06-08 16:55:15 +1000

    Update JS samples to use latest API and best practice error handling
    - Resolves #71

--- sample/HelloWorld/static/js/helloworld.js
+++ sample/HelloWorld/static/js/helloworld.js
@@ -1,8 +1,8 @@
 // Initialize an OpenTok Session object
-var session = TB.initSession(sessionId);
+var session = OT.initSession(apiKey, sessionId);
 
 // Initialize a Publisher, and place it into the element with id="publisher"
-var publisher = TB.initPublisher(apiKey, 'publisher');
+var publisher = OT.initPublisher('publisher');
 
 // Attach event handlers
 session.on({
@@ -11,7 +11,11 @@ session.on({
   sessionConnected: function(event) {
     // Publish the publisher we initialzed earlier (this will trigger 'streamCreated' on other
     // clients)
-    session.publish(publisher);
+    session.publish(publisher, function(error) {
+      if (error) {
+        console.error('Failed to publish', error);
+      }
+    });
   },
 
   // This function runs when another client publishes a stream (eg. session.publish())
@@ -23,10 +27,18 @@ session.on({
     document.getElementById('subscribers').appendChild(subContainer);
 
     // Subscribe to the stream that caused this event, put it inside the container we just made
-    session.subscribe(event.stream, subContainer);
+    session.subscribe(event.stream, subContainer, function(error) {
+      if (error) {
+        console.error('Failed to subscribe', error);
+      }
+    });
   }
 
 });
 
 // Connect to the Session using the 'apiKey' of the application and a 'token' for permission
-session.connect(apiKey, token);
+session.connect(token, function(error) {
+  if (error) {
+    console.error('Failed to connect', error);
+  }
+});
