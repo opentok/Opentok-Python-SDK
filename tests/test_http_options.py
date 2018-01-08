7d2d7ff0f7036d53aa2d1aebf6bec2996fdab357
Tyler Gould <gouldt@gmail.com>
2018-01-08 15:18:38 -0600

    test timeout http option

--- tests/test_http_options.py
+++ tests/test_http_options.py
@@ -1,11 +1,12 @@
 import unittest
-from six import text_type, u, b, PY2, PY3
-from six.moves.urllib.parse import parse_qs
 from nose.tools import raises
-from sure import expect
 import httpretty
+import requests
 
-from opentok import OpenTok, Session, MediaModes, OpenTokException, __version__
+from opentok import OpenTok, OpenTokException
+
+def _raise_timeout(*args):
+    raise requests.Timeout('Timeout occurred')
 
 class OpenTokSessionCreationTest(unittest.TestCase):
     def setUp(self):
@@ -13,17 +14,14 @@ class OpenTokSessionCreationTest(unittest.TestCase):
         self.api_secret = u('1234567890abcdef1234567890abcdef1234567890')
         httpretty.enable()
         httpretty.register_uri(httpretty.POST, u('https://api.opentok.com/session/create'),
-                               body=u('<?xml version="1.0" encoding="UTF-8" standalone="yes"?><sessions><Session><session_id>1_MX4xMjM0NTZ-fk1vbiBNYXIgMTcgMDA6NDE6MzEgUERUIDIwMTR-MC42ODM3ODk1MzQ0OTQyODA4fg</session_id><partner_id>123456</partner_id><create_dt>Mon Mar 17 00:41:31 PDT 2014</create_dt></Session></sessions>'),
+                               body=_raise_timeout,
                                status=200,
                                content_type=u('text/xml'))
 
     def tearDown(self):
         httpretty.disable()
 
+    @raises(OpenTokException)
     def test_timeout(self):
-        # TODO: tell httpretty to timeout
-        opentok = OpenTok(self.api_key, self.api_secret, timeout=5)
-
-        session = opentok.create_session()
-
-        # TODO: expect an exception
+        opentok = OpenTok(self.api_key, self.api_secret, timeout=1)
+        opentok.create_session()
