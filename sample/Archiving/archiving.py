5322fc90c80eefd5c056126720f62c903911fcbb
superdiana <diana.rodriguezmanrique@vonage.com>
2020-12-09 21:39:30 -0500

    implement jwt generation improvements

--- sample/Archiving/archiving.py
+++ sample/Archiving/archiving.py
@@ -4,74 +4,96 @@ from email.utils import formatdate
 import os, time
 
 try:
-    api_key = os.environ['API_KEY']
-    api_secret = os.environ['API_SECRET']
+    api_key = os.environ["API_KEY"]
+    api_secret = os.environ["API_SECRET"]
 except Exception:
-    raise Exception('You must define API_KEY and API_SECRET environment variables')
+    raise Exception("You must define API_KEY and API_SECRET environment variables")
 
 app = Flask(__name__)
 opentok = OpenTok(api_key, api_secret)
 session = opentok.create_session(media_mode=MediaModes.routed)
 
-@app.template_filter('datefmt')
+
+@app.template_filter("datefmt")
 def datefmt(dt):
     return formatdate(time.mktime(dt.timetuple()))
 
+
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+
 
 @app.route("/host")
 def host():
     key = api_key
     session_id = session.session_id
     token = opentok.generate_token(session_id)
-    return render_template('host.html', api_key=key, session_id=session_id, token=token)
+    return render_template("host.html", api_key=key, session_id=session_id, token=token)
+
 
 @app.route("/participant")
 def participant():
     key = api_key
     session_id = session.session_id
     token = opentok.generate_token(session_id)
-    return render_template('participant.html', api_key=key, session_id=session_id, token=token)
+    return render_template(
+        "participant.html", api_key=key, session_id=session_id, token=token
+    )
+
 
 @app.route("/history")
 def history():
-    page = int(request.args.get('page', '1'))
+    page = int(request.args.get("page", "1"))
     offset = (page - 1) * 5
     archives = opentok.get_archives(offset=offset, count=5)
 
-    show_previous = '/history?page=' + str(page-1) if page > 1 else None
-    show_next = '/history?page=' + str(page+1) if archives.count > (offset + 5) else None
+    show_previous = "/history?page=" + str(page - 1) if page > 1 else None
+    show_next = (
+        "/history?page=" + str(page + 1) if archives.count > (offset + 5) else None
+    )
 
-    return render_template('history.html', archives=archives, show_previous=show_previous,
-                            show_next=show_next)
+    return render_template(
+        "history.html",
+        archives=archives,
+        show_previous=show_previous,
+        show_next=show_next,
+    )
+
 
 @app.route("/download/<archive_id>")
 def download(archive_id):
     archive = opentok.get_archive(archive_id)
     return redirect(archive.url)
 
-@app.route("/start", methods=['POST'])
+
+@app.route("/start", methods=["POST"])
 def start():
-    has_audio = 'hasAudio' in request.form.keys()
-    has_video = 'hasVideo' in request.form.keys()
-    output_mode = OutputModes[request.form.get('outputMode')]
-    resolution = request.form.get('resolution')
-    archive = opentok.start_archive(session.session_id, name="Python Archiving Sample App",
-                                    has_audio=has_audio, has_video=has_video,
-                                    output_mode=output_mode, resolution=resolution)
+    has_audio = "hasAudio" in request.form.keys()
+    has_video = "hasVideo" in request.form.keys()
+    output_mode = OutputModes[request.form.get("outputMode")]
+    resolution = request.form.get("resolution")
+    archive = opentok.start_archive(
+        session.session_id,
+        name="Python Archiving Sample App",
+        has_audio=has_audio,
+        has_video=has_video,
+        output_mode=output_mode,
+        resolution=resolution,
+    )
     return archive.json()
+
 
 @app.route("/stop/<archive_id>")
 def stop(archive_id):
     archive = opentok.stop_archive(archive_id)
     return archive.json()
 
+
 @app.route("/delete/<archive_id>")
 def delete(archive_id):
     opentok.delete_archive(archive_id)
-    return redirect(url_for('history'))
+    return redirect(url_for("history"))
 
 
 if __name__ == "__main__":
--- tests/test_sip_call.py
+++ tests/test_sip_call.py
@@ -8,93 +8,115 @@ from opentok import OpenTok, SipCall, __version__
 
 from .validate_jwt import validate_jwt_header
 
+
 class OpenTokSipCallTest(unittest.TestCase):
     def setUp(self):
-        self.api_key = u('123456')
-        self.api_secret = u('1234567890abcdef1234567890abcdef1234567890')
+        self.api_key = u("123456")
+        self.api_secret = u("1234567890abcdef1234567890abcdef1234567890")
         self.opentok = OpenTok(self.api_key, self.api_secret)
 
-        self.session_id = u('SESSIONID')
-        self.token = u('TOKEN')
-        self.sip_uri = u('sip:user@sip.partner.com;transport=tls')
+        self.session_id = u("SESSIONID")
+        self.token = u("TOKEN")
+        self.sip_uri = u("sip:user@sip.partner.com;transport=tls")
 
     @httpretty.activate
     def test_sip_call_with_required_parameters(self):
         """
         Test dial() method using just the required parameters: session_id, token, sip_uri
         """
-        sip_call = SipCall({
-            u('id'): u('b0a5a8c7-dc38-459f-a48d-a7f2008da853'),
-            u('connectionId'): u('e9f8c166-6c67-440d-994a-04fb6dfed007'),
-            u('streamId'): u('482bce73-f882-40fd-8ca5-cb74ff416036')
-        })
+        sip_call = SipCall(
+            {
+                u("id"): u("b0a5a8c7-dc38-459f-a48d-a7f2008da853"),
+                u("connectionId"): u("e9f8c166-6c67-440d-994a-04fb6dfed007"),
+                u("streamId"): u("482bce73-f882-40fd-8ca5-cb74ff416036"),
+            }
+        )
 
         httpretty.register_uri(
             httpretty.POST,
-            u('https://api.opentok.com/v2/project/{0}/dial').format(self.api_key),
-            body=textwrap.dedent(u("""\
-                {
-                    "id": "b0a5a8c7-dc38-459f-a48d-a7f2008da853",
-                    "connectionId": "e9f8c166-6c67-440d-994a-04fb6dfed007",
-                    "streamId": "482bce73-f882-40fd-8ca5-cb74ff416036"
-                }""")),
+            u("https://api.opentok.com/v2/project/{0}/dial").format(self.api_key),
+            body=textwrap.dedent(
+                u(
+                    """\
+                        {
+                            "id": "b0a5a8c7-dc38-459f-a48d-a7f2008da853",
+                            "connectionId": "e9f8c166-6c67-440d-994a-04fb6dfed007",
+                            "streamId": "482bce73-f882-40fd-8ca5-cb74ff416036"
+                        }
+                    """
+                )
+            ),
             status=200,
-            content_type=u('application/json')
+            content_type=u("application/json"),
         )
 
         sip_call_response = self.opentok.dial(self.session_id, self.token, self.sip_uri)
-        validate_jwt_header(self, httpretty.last_request().headers[u('x-opentok-auth')])
-        expect(httpretty.last_request().headers[u('user-agent')]).to(contain(
-            u('OpenTok-Python-SDK/')+__version__))
-        expect(httpretty.last_request().headers[u('content-type')]).to(equal(u('application/json')))
+        validate_jwt_header(self, httpretty.last_request().headers[u("x-opentok-auth")])
+        expect(httpretty.last_request().headers[u("user-agent")]).to(
+            contain(u("OpenTok-Python-SDK/") + __version__)
+        )
+        expect(httpretty.last_request().headers[u("content-type")]).to(
+            equal(u("application/json"))
+        )
         expect(sip_call_response).to(be_an(SipCall))
-        expect(sip_call_response).to(have_property(u('id'), sip_call.id))
-        expect(sip_call_response).to(have_property(u('connectionId'), sip_call.connectionId))
-        expect(sip_call_response).to(have_property(u('streamId'), sip_call.streamId))
+        expect(sip_call_response).to(have_property(u("id"), sip_call.id))
+        expect(sip_call_response).to(
+            have_property(u("connectionId"), sip_call.connectionId)
+        )
+        expect(sip_call_response).to(have_property(u("streamId"), sip_call.streamId))
 
     @httpretty.activate
     def test_sip_call_with_aditional_options(self):
         """
         Test dial() method with aditional options
         """
-        sip_call = SipCall({
-            u('id'): u('b0a5a8c7-dc38-459f-a48d-a7f2008da853'),
-            u('connectionId'): u('e9f8c166-6c67-440d-994a-04fb6dfed007'),
-            u('streamId'): u('482bce73-f882-40fd-8ca5-cb74ff416036')
-        })
+        sip_call = SipCall(
+            {
+                u("id"): u("b0a5a8c7-dc38-459f-a48d-a7f2008da853"),
+                u("connectionId"): u("e9f8c166-6c67-440d-994a-04fb6dfed007"),
+                u("streamId"): u("482bce73-f882-40fd-8ca5-cb74ff416036"),
+            }
+        )
 
         httpretty.register_uri(
             httpretty.POST,
-            u('https://api.opentok.com/v2/project/{0}/dial').format(self.api_key),
-            body=textwrap.dedent(u("""\
-                {
-                    "id": "b0a5a8c7-dc38-459f-a48d-a7f2008da853",
-                    "connectionId": "e9f8c166-6c67-440d-994a-04fb6dfed007",
-                    "streamId": "482bce73-f882-40fd-8ca5-cb74ff416036"
-                }""")),
+            u("https://api.opentok.com/v2/project/{0}/dial").format(self.api_key),
+            body=textwrap.dedent(
+                u(
+                    """\
+                        {
+                            "id": "b0a5a8c7-dc38-459f-a48d-a7f2008da853",
+                            "connectionId": "e9f8c166-6c67-440d-994a-04fb6dfed007",
+                            "streamId": "482bce73-f882-40fd-8ca5-cb74ff416036"
+                        }
+                    """
+                )
+            ),
             status=200,
-            content_type=u('application/json')
+            content_type=u("application/json"),
         )
 
         # aditional options to establish the sip call
         options = {
-            'from': 'from@example.com',
-            'headers': {
-                'headerKey': 'headerValue'
-            },
-            'auth': {
-                'username': 'username',
-                'password': 'password'
-            },
-            'secure': True
+            "from": "from@example.com",
+            "headers": {"headerKey": "headerValue"},
+            "auth": {"username": "username", "password": "password"},
+            "secure": True,
         }
 
-        sip_call_response = self.opentok.dial(self.session_id, self.token, self.sip_uri, options)
-        validate_jwt_header(self, httpretty.last_request().headers[u('x-opentok-auth')])
-        expect(httpretty.last_request().headers[u('user-agent')]).to(contain(
-            u('OpenTok-Python-SDK/')+__version__))
-        expect(httpretty.last_request().headers[u('content-type')]).to(equal(u('application/json')))
+        sip_call_response = self.opentok.dial(
+            self.session_id, self.token, self.sip_uri, options
+        )
+        validate_jwt_header(self, httpretty.last_request().headers[u("x-opentok-auth")])
+        expect(httpretty.last_request().headers[u("user-agent")]).to(
+            contain(u("OpenTok-Python-SDK/") + __version__)
+        )
+        expect(httpretty.last_request().headers[u("content-type")]).to(
+            equal(u("application/json"))
+        )
         expect(sip_call_response).to(be_an(SipCall))
-        expect(sip_call_response).to(have_property(u('id'), sip_call.id))
-        expect(sip_call_response).to(have_property(u('connectionId'), sip_call.connectionId))
-        expect(sip_call_response).to(have_property(u('streamId'), sip_call.streamId))
+        expect(sip_call_response).to(have_property(u("id"), sip_call.id))
+        expect(sip_call_response).to(
+            have_property(u("connectionId"), sip_call.connectionId)
+        )
+        expect(sip_call_response).to(have_property(u("streamId"), sip_call.streamId))
