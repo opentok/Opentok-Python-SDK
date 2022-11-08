595c5d7629b43bf38c8ef517b62840b641fd52ce
maxkahan <maxkahan94@gmail.com>
2022-11-08 18:35:11 +0000

    renaming webhook_audio_connection module -> websocket_audio_connection

---
+++ opentok/websocket_audio_connection.py
@@ -0,0 +1,20 @@
+import json
+from six import iteritems
+
+class WebsocketAudioConnection:
+    """Represents information about the audio streaming of an OpenTok session to a websocket."""
+
+    def __init__(self, kwargs):
+        self.id = kwargs.get("id")
+        self.connectionId = kwargs.get("connectionId")
+
+    def json(self):
+        """Returns a JSON representation of the websocket audio connection information."""
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+
+    def attrs(self):
+        """
+        Returns a dictionary of the websocket audio connection's attributes.
+        """
+        return dict((k, v) for k, v in iteritems(self.__dict__))
+        
